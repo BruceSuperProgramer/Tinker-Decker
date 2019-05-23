37688228474d2be172e276843b42381bd14a30f1
bruce <bruce.h@bmyg.com.au>
2019-05-23 15:07:23 +1000

    Initial commit

---
+++ App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Analytics from 'appcenter-analytics';
+import codePush from "react-native-code-push";
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+class App extends Component<Props> {
+
+  async componentDidMount(){
+    await Analytics.setEnabled(true);
+
+    Analytics.trackEvent('App page loaded', { Category: 'App page', FileName: 'App main page' });
+
+    Analytics.trackEvent('Video clicked');
+
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const codePushOptiosn = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_START
+}
+
+export default codePush(codePushOptiosn)(App);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
